058d4e2ff3d8dfb3aa01e37eee1d90745d928503
Diego Armando Castellanos Sabogal <Diego Castellanos>
2017-07-06 20:05:22 -0500

    Se modifican los terminos y condiciones

--- frontend/modules/quote/views/quote/exportQuoteNaturalPerson/_conditionsQuoteNaturalPerson.php
+++ frontend/modules/quote/views/quote/exportQuoteNaturalPerson/_conditionsQuoteNaturalPerson.php
@@ -10,6 +10,6 @@ use yii\helpers\Html;
 <ul>
     <li>Creemos que las estimaciones de tiempo pueden variar dependiendo de diferentes factores.</li>
     <li>Valoramos tu trabajo y esperando que valores el nuestro, requerimos de un porcentaje inicial de la cotización para comenzar a crear tu idea.</li>
-    <li>Creemos en tu fidelidad y agradecemos que creas en nosotros, por eso te damos el beneficio PUNTOSAGILES. Acumulables y redimibles en dinero o tiempo.</li>
+    <!--<li>Creemos en tu fidelidad y agradecemos que creas en nosotros, por eso te damos el beneficio PUNTOSAGILES. Acumulables y redimibles en dinero o tiempo.</li>-->
     <li>Podremos mantenerte esta cotización por un tiempo de hasta 3 meses. No te preocupes pasado este tiempo podremos evaluar juntos este costo. </li>
 </ul>
